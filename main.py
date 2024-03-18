fb1812010bdbe90d01c7f3663f0d85516ce0e04f
Monteiro-gh <161480035+Monteiro-gh@users.noreply.github.com>
2024-03-18 11:10:43 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print("==============================Agenda de Contatos===================================")
+
+opcoes = int(input("1.novo contato \n2.listar contato \n3.pesquisar contato por número \n4.excluir contato \n5.atualizar contato \n6.sair \n=> "))
+
+if opcoes == 1:
+    print("")
+elif opcoes == 2:
